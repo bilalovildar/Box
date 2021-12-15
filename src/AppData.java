d59f6b1a9575e0302670dc6c877a8b3f30f2c962
bilalov.ild@gmail.com <Camelion100mAh>
2021-12-15 19:28:30 +0300

    commit Appdata less6

---
+++ src/AppData.java
@@ -0,0 +1,36 @@
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.io.*;
+
+public class AppData implements Serializable {
+    String[] header;
+    int[][] data;
+
+    public AppData(String[] header, int[][] data) {
+        this.header = header;
+        this.data = data;
+    }
+
+    public static void main(String[] args) {
+        String[] header = new String[]{"Value 1", "Value 2", "Value 3"};
+        int[][] data = new int[][]{{100, 200, 123}, {300, 400, 500}};
+        AppData appData = new AppData(header, data);
+        //сериализация
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("test.csv"))) {
+            objectOutputStream.writeObject(appData);
+            objectOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //десериализация
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("test.csv"))) {
+            AppData appDataResulted = (AppData) objectInputStream.readObject();
+            objectInputStream.close();
+            System.out.println(appDataResulted);
+        } catch (IOException | ClassNotFoundException ex) {
+            ex.printStackTrace();
+
+        }
+
+    }
+}
